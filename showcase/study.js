daed9a3b078aa1fae50fe216fe364ed8dd1e8e2e
Richie Hsieh <whitedogg13@gmail.com>
2018-06-04 17:05:59 +0800

    Add some study stuff

--- showcase/study.js
+++ showcase/study.js
@@ -9,71 +9,76 @@ import {
   VerticalBarSeriesCanvas,
   VerticalRectSeries,
   LineSeries,
+  MarkSeries,
 } from 'index'; // this should be 'react-vis' for a real app
 
-console.log( VerticalRectSeries);
-
-function getData() {
-    return {
-        "chart": {
-            "type": "block"
-        },
-        "title": {
-            "text": "E30+3ST"
-        },
-        "subtitle": {
-            "text": "訓練說明"
-        },
-        "yAxis": {
-            "text": "HRR心率(%)",
-            "tickInterval": 20
-        },
-        "xAxis": {
-            "text": "時間(分鐘)",
-            "tickInterval": 10
-        },
-        "series": {
-            "data": [{
-                "xStart": 0,
-                "xEnd": 10,
-                "yStart": 59,
-                "yEnd": 74,
-                "color": "#5555FF"
-            }, {
-                "xStart": 10,
-                "xEnd": 20,
-                "yStart": 59,
-                "yEnd": 74,
-                "color": "#5555FF"
-            }, {
-                "xStart": 20,
-                "xEnd": 30,
-                "yStart": 40,
-                "yEnd": 50,
-                "color": "#5555FF"
-            }, {
-                "xStart": 30,
-                "xEnd": 40,
-                "yStart": 60,
-                "yEnd": 80,
-                "color": "#5555FF"
-            }, {
-                "xStart": 40,
-                "xEnd": 50,
-                "yStart": 70,
-                "yEnd": 90,
-                "color": "#5555FF",
-            }]
-        }
+const jsonData = {
+    "chart": {
+        "type": "block"
+    },
+    "title": {
+        "text": "E30+3ST"
+    },
+    "subtitle": {
+        "text": "訓練說明"
+    },
+    "xMin": 0,
+    "xMax": 100,
+    "yMin": 0,
+    "yMax": 100,
+    "yAxis": {
+        "text": "HRR心率(%)",
+        "tickInterval": 20
+    },
+    "xAxis": {
+        "text": "時間(分鐘)",
+        "tickInterval": 10
+    },
+    "series": {
+        "data": [{
+            "xStart": 0,
+            "xEnd": 10,
+            "yStart": 59,
+            "yEnd": 74,
+            "color": "#5555FF"
+        }, {
+            "xStart": 10,
+            "xEnd": 20,
+            "yStart": 25,
+            "yEnd": 48,
+            "color": "#5555FF"
+        }, {
+            "xStart": 20,
+            "xEnd": 30,
+            "yStart": 40,
+            "yEnd": 50,
+            "color": "#5555FF"
+        }, {
+            "xStart": 30,
+            "xEnd": 40,
+            "yStart": 60,
+            "yEnd": 80,
+            "color": "#5555FF"
+        }, {
+            "xStart": 40,
+            "xEnd": 50,
+            "yStart": 70,
+            "yEnd": 90,
+            "color": "#5555FF",
+        }]
     }
 };
 
-/**
- * xType = "ordinal" => such as its 'A', 'B', 'C'
- */
 class Study extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: null
+        }
+    }
+
     render() {
-        let chartInfo = getData();
+        let chartInfo = jsonData;
         let rectData = chartInfo.series.data.map(
             cell => {
                 return {
@@ -94,9 +99,13 @@ class Study extends Component {
             }
         );
 
+        let {index} = this.state;
+
         return (
             <div>
                 <XYPlot
+                    xDomain={[chartInfo.xMin, chartInfo.xMax]}
+                    yDomain={[chartInfo.yMin, chartInfo.yMax]}
                     width={300}
                     height={300}
                 >
@@ -104,16 +113,41 @@ class Study extends Component {
                     <HorizontalGridLines />
                     <XAxis />
                     <YAxis />
+
                     <VerticalRectSeries 
-                        stroke="red"
+                        stroke={index === "red"}
                         data={rectData}
                     />                    
                     <LineSeries
                         data={lineData}
+                        onNearestX={this._onNearestX}
+                    />
+
+                    {
+                        index !== null && (
+                            <LineSeries
+                                data={[
+                                    {x: lineData[index].x, y: chartInfo.yMin},
+                                    {x: lineData[index].x, y: chartInfo.yMax}
+                                ]}
+                            />
+                        )
+                    }
+
+                    <MarkSeries
+                      data={lineData}
+                      onValueClick={
+                          e => console.log(e)
+                      }
                     />
                 </XYPlot>
             </div>
         );
+    }
+
+    _onNearestX = (_, {index}) => {
+        console.log(index);
+        this.setState({index});
     }
 }
 
